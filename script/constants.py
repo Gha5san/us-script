266027285b0a358c34e3dd937ebd6fcc7c23e3cb
Ghassan Al Kulaibi <asgh1234515@gmail.com>
2023-12-09 12:57:38 +0000

    Add simple user stories generation gpt-3.5 script

---
+++ script/constants.py
@@ -0,0 +1,26 @@
+LABEL_DESCRIPTION = {
+    'Compatibility': 'Focuses on ensuring that a product or system can work effectively across different environments, devices, or software versions. Involves integration, cross-platform functionality, and adaptability to various technical specifications.',
+    'Functional': 'Relates to the basic operations and functionalities of a system or product. Includes the development or enhancement of specific features, capabilities, or processes essential for the system\'s primary tasks.',
+    'Maintainability': 'Involves aspects of a system that make it easier to maintain, update, and manage. Emphasizes clarity of code, ease of identifying and fixing issues, and the ability to adapt to changes without significant rework.',
+    'Performance': 'Focuses on the efficiency and responsiveness of a system. Addresses issues like speed, resource management, and the system\'s ability to handle high loads or complex operations without degradation in quality or speed.',
+    'Portability': 'Deals with the ability of software or applications to be used in different environments or on various platforms. Concerns the system\'s adaptability to different devices, operating systems, or browsers.',
+    'Reliability': 'Emphasizes the stability and dependability of a system. Includes error handling, consistent performance under various conditions, and accuracy in delivering expected results.',
+    'Security': 'Pertains to the protection of systems and data from unauthorized access, breaches, or other security threats. Involves encryption, authentication, data integrity, and compliance with security standards.',
+    'Usability': 'Focuses on the user experience and ease of use of a system or product. Involves user interface design, user interaction workflows, accessibility, and overall user satisfaction.'
+}
+
+
+ # system_message = (
+    # "You are an advanced AI specializing in software development and Agile Scrum practices. "
+    # "Your challenge is to invent a unique, concise, and clear user story that strictly adheres to Agile principles. "
+    # "Focus on crafting a story that revolves around a singular, actionable item offering distinct value to the end user. "
+    # "The story should follow the format: 'As a [type of user], I want [an action or feature], so that [benefit or value].' "
+    # "Aim for creativity and originality, ensuring the story is specific, measurable, achievable, relevant, and time-bound (SMART). "
+    # "Each story should uniquely reflect the essence of the given label, avoiding repetition of concepts or scenarios previously used."
+    # )
+    
+    # user_message = (
+    # f"Category: {label}\nDefinition: {description}\n"
+    # "Create a fresh user story based on this category and definition. "
+    # "Think outside the box, exploring new angles and perspectives that have not been covered in typical user stories under this label."
+    # )
